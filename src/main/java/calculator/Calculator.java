2d0dfad7e40e8ba83bb8a5401795f86f00e1dfdf
praveenreddychalamalla <praveenreddychalamalla@gmail.com>
2022-04-18 13:09:28 +0530

    Monitoring

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,20 +1,25 @@
 package calculator;
 
+import com.google.common.math.BigIntegerMath;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.math.BigInteger;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
     public Calculator() {
     }
-
+    private static final Logger logger=LogManager.getLogger(Calculator.class);
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         double number1, number2;
         do {
 
-            System.out.println("DevOps - Calculator, Choose to perform operation");
-            System.out.print("Press 1 to Add\nPress 2 to Subtract\nPress 3 to Multiply\nPress 4 to Divide\n" +
+            System.out.println("DevOps - Scientific Calculator, Choose to perform operation");
+            System.out.print("1.Factorial\n2.Square Root\n3.Natural Logarithm\n4.Power\n" +
                     "Press any other key to exit\nEnter your choice: ");
             int choice;
             try {
@@ -22,27 +27,48 @@ public class Calculator {
             } catch (InputMismatchException error) {
                 return;
             }
-            try {
-                System.out.print("Enter the first number : ");
-                number1 = scanner.nextDouble();
-                System.out.print("Enter the second number : ");
-                number2 = scanner.nextDouble();
-            } catch (InputMismatchException error) {
-                System.out.println("Invalid input, Entered input is not a number");
-                return;
-            }
             switch (choice) {
                 case 1:
-                    System.out.println("Addition result is : " + calculator.add(number1, number2));
+                    try {
+                        System.out.print("Enter the number : ");
+                        number1 = scanner.nextDouble();
+                    } catch (InputMismatchException error) {
+                        System.out.println("Invalid input, Entered input is not a number");
+                        return;
+                    }
+                    System.out.println("Result : " + calculator.factorial(number1));
                     break;
                 case 2:
-                    System.out.println("Subtraction result is : " + calculator.subtract(number1, number2));
+                    try {
+                        System.out.print("Enter the number : ");
+                        number1 = scanner.nextDouble();
+                    } catch (InputMismatchException error) {
+                        System.out.println("Invalid input, Entered input is not a number");
+                        return;
+                    }
+                    System.out.println("Result : " + calculator.squareRoot(number1));
                     break;
                 case 3:
-                    System.out.println("Multiplication result is : " + calculator.multiply(number1, number2));
+                    try {
+                        System.out.print("Enter the number : ");
+                        number1 = scanner.nextDouble();
+                    } catch (InputMismatchException error) {
+                        System.out.println("Invalid input, Entered input is not a number");
+                        return;
+                    }
+                    System.out.println("Result : " + calculator.naturalLogarithm(number1));
                     break;
                 case 4:
-                    System.out.println("Division result is : " + calculator.divide(number1, number2));
+                    try {
+                        System.out.print("Enter the Base : ");
+                        number1 = scanner.nextDouble();
+                        System.out.print("Enter the Exponent : ");
+                        number2 = scanner.nextDouble();
+                    } catch (InputMismatchException error) {
+                        System.out.println("Invalid input, Entered input is not a number");
+                        return;
+                    }
+                    System.out.println("Result : " + calculator.power(number1,number2));
                     break;
                 default:
                     System.out.println("Exiting....");
@@ -51,30 +77,32 @@ public class Calculator {
         } while (true);
     }
 
-    public double add(double number1, double number2) {
-        System.out.println("Addition of " + number1 + ", " + number2);
-        double result = number1 + number2;
-        System.out.println("RESULT = " + result);
-        return result;
+    public double power(double number1, double number2) {
+        logger.info("[Power] - "+number1+" "+number2);
+        double res=Math.pow(number1,number2);
+        logger.info("[Power Result] - "+res);
+        return res;
     }
 
-    public double subtract(double number1, double number2) {
-        System.out.println("Subtraction of " + number1 + ", " + number2);
-        double result = number1 - number2;
-        System.out.println("RESULT = " + result);
-        return result;
-    }
-    public double multiply(double number1, double number2) {
-        System.out.println("Multiplication of " + number1 + ", " + number2);
-        double result = number1 * number2;
-        System.out.println("RESULT = " + result);
-        return result;
+    public double naturalLogarithm(double number) {
+        logger.info("[Natural Logarithm] - "+number);
+        double res=Math.log(number);
+        logger.info("[Natural Logarithm Result] - "+res);
+        return res;
     }
 
-    public double divide(double number1, double number2) {
-        System.out.println("Division of " + number1 + ", " + number2);
-        double result = number1 / number2;
-        System.out.println("RESULT = " + result);
-        return result;
+    public double squareRoot(double number) {
+        logger.info("[Square Root] - "+number);
+        double res=Math.sqrt(number);
+        logger.info("[Square Root Result] - "+res);
+        return res;
     }
+
+    public BigInteger factorial(double number) {
+        logger.info("[Factorial] - "+number);
+        BigInteger res = BigIntegerMath.factorial((int)number);
+        logger.info("[Factorial Result] - "+res);
+        return res;
+    }
+
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,11 @@
 package calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
@@ -9,50 +14,50 @@ public class CalculatorTest {
     Calculator calculator=new Calculator();
 
     @Test
-    public void addingTruePositive(){
-        assertEquals("Adding two integers for true positive",8,calculator.add(3,5),DELTA);
-        assertEquals("Adding two doubles for true positive",30.23,calculator.add(30.1,0.13),DELTA);
+    public void factorialTruePositive(){
+        assertEquals("Factorial for true positive",120,calculator.factorial(5).doubleValue(),DELTA);
+        assertEquals("Factorial for true positive",24,calculator.factorial(4).doubleValue(),DELTA);
     }
 
     @Test
-    public void addingFalsePositive(){
-        assertNotEquals("Adding two integers for false positive",10,calculator.add(2,6),DELTA);
-        assertNotEquals("Adding two doubles for false positive",3.6,calculator.add(0.1,0.8),DELTA);
+    public void factorialFalsePositive(){
+        assertNotEquals("Factorial for false positive",1,calculator.factorial(3).doubleValue(),DELTA);
+        assertNotEquals("Factorial for false positive",100,calculator.factorial(6).doubleValue(),DELTA);
     }
 
     @Test
-    public void subtractingTruePositive(){
-        assertEquals("Subtracting two integers for true positive",35,calculator.subtract(45,10),DELTA);
-        assertEquals("Subtracting two doubles for true positive",50.50,calculator.subtract(51.0,0.5),DELTA);
+    public void squareRootTruePositive(){
+        assertEquals("Square Root for true positive",8,calculator.squareRoot(64),DELTA);
+        assertEquals("Square Root for true positive",10,calculator.squareRoot(100),DELTA);
     }
 
     @Test
-    public void subtractingFalsePositive(){
-        assertNotEquals("Subtracting two integers for false positive",19,calculator.subtract(3,11),DELTA);
-        assertNotEquals("Subtracting two doubles for false positive",10,calculator.subtract(100.3,2.6),DELTA);
+    public void squareRootFalsePositive(){
+        assertNotEquals("Square Root for false positive",5,calculator.squareRoot(625),DELTA);
+        assertNotEquals("Square Root for false positive",4,calculator.squareRoot(256),DELTA);
     }
 
     @Test
-    public void multiplyingTruePositive(){
-        assertEquals("Multiplying two integers for true positive",40,calculator.multiply(5,8),DELTA);
-        assertEquals("Multiplying two doubles for true positive",6.6,calculator.multiply(3.3,2.0),DELTA);
+    public void naturalLogarithmTruePositive(){
+        assertEquals("Natural Logarithm for true positive",1.6094379124341003,calculator.naturalLogarithm(5),DELTA);
+        assertEquals("Natural Logarithm for true positive",0,calculator.naturalLogarithm(1),DELTA);
     }
 
     @Test
-    public void multiplyingFalsePositive(){
-        assertNotEquals("Multiplying two integers for false positive",500,calculator.multiply(2,8),DELTA);
-        assertNotEquals("Multiplying two doubles for false positive",1000.5,calculator.multiply(10.3,5.6),DELTA);
+    public void naturalLogarithmFalsePositive(){
+        assertNotEquals("Natural Logarithm for false positive",50,calculator.naturalLogarithm(6),DELTA);
+        assertNotEquals("Natural Logarithm for false positive",10,calculator.naturalLogarithm(100),DELTA);
     }
 
     @Test
-    public void dividingTruePositive(){
-        assertEquals("Dividing two integers for true positive",2,calculator.divide(8,4),DELTA);
-        assertEquals("Dividing two doubles for true positive",10.1,calculator.divide(50.5,5),DELTA);
+    public void powerTruePositive(){
+        assertEquals("Power for true positive",1024,calculator.power(2,10),DELTA);
+        assertEquals("Power for true positive",81,calculator.power(3,4),DELTA);
     }
 
     @Test
-    public void dividingFalsePositive(){
-        assertNotEquals("Dividing two integers for false positive",20,calculator.multiply(34,7),DELTA);
-        assertNotEquals("Dividing two doubles for false positive",250.1,calculator.multiply(2,5.65),DELTA);
+    public void powerFalsePositive(){
+        assertNotEquals("Power for false positive",20,calculator.power(2,10),DELTA);
+        assertNotEquals("Power for false positive",10,calculator.power(5,4),DELTA);
     }
 }
